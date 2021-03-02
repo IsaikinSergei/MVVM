8419425c3a1c484cfd9a71a6ec97596392298ef6
IsaikinSergei <akinfest007@gmail.com>
2021-03-02 13:35:20 +0400

    Initial commit

---
+++ MVVM-2/Model/Profile.swift
@@ -0,0 +1,14 @@
+//
+//  Profile.swift
+//  MVVM-2
+//
+//  Created by Sergei Isaikin on 02.03.2021.
+//
+
+import Foundation
+
+struct Profile {
+    var name: String
+    var secondName: String
+    var age: Int
+}
---
+++ MVVM-2/View/TableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  TableViewCell.swift
+//  MVVM-2
+//
+//  Created by Sergei Isaikin on 02.03.2021.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    @IBOutlet weak var fullNameLabel: UILabel!
+    @IBOutlet weak var ageLabel: UILabel!
+    
+}
