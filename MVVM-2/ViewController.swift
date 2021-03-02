b5d1fdc90e88f3c245490a2c0653112a16345046
IsaikinSergei <akinfest007@gmail.com>
2021-03-02 12:46:58 +0400

    Initial Commit

---
+++ MVVM-2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM-2
+//
+//  Created by Sergei Isaikin on 02.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
